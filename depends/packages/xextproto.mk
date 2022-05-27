965ecb59f51982f74d08173f7ea3f95ae63737f6
carter <cartersullivanswim@gmail.com>
2022-05-26 21:44:02 -0500

    still trying to fix github build stuff

--- depends/packages/xextproto.mk
+++ depends/packages/xextproto.mk
@@ -1,11 +1,11 @@
-package=expat
-$(package)_version=2.2.1
-$(package)_download_path=https://downloads.sourceforge.net/project/expat/expat/$($(package)_version)
+package=xextproto
+$(package)_version=7.3.0
+$(package)_download_path=http://xorg.freedesktop.org/releases/individual/proto
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=1868cadae4c82a018e361e2b2091de103cd820aaacb0d6cfa49bd2cd83978885
+$(package)_sha256_hash=f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static
+$(package)_config_opts=--disable-shared
 endef
 
 define $(package)_config_cmds
